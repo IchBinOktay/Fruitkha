3df4fd02b17c23a8cd51d09818b3a062eaf6685b
Oktay Babayev <ogtaypb@code.edu.az>
2025-01-05 12:16:45 +0400

    Done.

---
+++ Data/Migrations/20250104063837_DatabaseAdded.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NewsId",
+                table: "Comments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_NewsId",
+                table: "Comments",
+                column: "NewsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments",
+                column: "NewsId",
+                principalTable: "News",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_News_NewsId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_NewsId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "NewsId",
+                table: "Comments");
+        }
+    }
+}
